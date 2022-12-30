b01da8090d6905fc35c32affa755ca06d3695978
robson <robsonmesquita-2011@outlook.com>
2022-12-30 06:19:20 -0300

    primeiro commit

--- client/script.js
+++ client/script.js
@@ -7,76 +7,84 @@ const chatContainer = document.querySelector('#chat_container')
 let loadInterval
 
 function loader(element) {
-  element.textContent = ''
+    element.textContent = ''
 
-  loadInterval = setInterval(() => {
-      element.textContent += '.';
+    loadInterval = setInterval(() => {
+       
+        element.textContent += '.';
 
-      if (element.textContent === '....') {
-          element.textContent = '';
-      }
-  }, 300);
+        
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
 }
 
 function typeText(element, text) {
-  let index = 0
+    let index = 0
 
-  let interval = setInterval(() => {
-      if (index < text.length) {
-          element.innerHTML += text.charAt(index)
-          index++
-      } else {
-          clearInterval(interval)
-      }
-  }, 20)
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
 }
 
-function generateUniqueId() {
-  const timestamp = Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timestamp}-${hexadecimalString}`;
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
-  return (
-      `
-      <div class="wrapper ${isAi && 'ai'}">
-          <div class="chat">
-              <div class="profile">
-                  <img 
-                    src=${isAi ? bot : user} 
-                    alt="${isAi ? 'bot' : 'user'}" 
-                  />
-              </div>
-              <div class="message" id=${uniqueId}>${value}</div>
-          </div>
-      </div>
-  `
-  )
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
 }
 
 const handleSubmit = async (e) => {
-  e.preventDefault()
+    e.preventDefault()
 
-  const data = new FormData(form)
+    const data = new FormData(form)
 
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-  form.reset()
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-  const uniqueId = generateUniqueId()
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-  chatContainer.scrollTop = chatContainer.scrollHeight;
+    form.reset()
 
-  const messageDiv = document.getElementById(uniqueId)
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-  loader(messageDiv)
 
-  const response = await fetch('https://codex-5mpc.onrender.com', {
-    method: 'POST',
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+
+    const messageDiv = document.getElementById(uniqueId)
+
+
+    loader(messageDiv)
+
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
@@ -90,7 +98,7 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim();
+        const parsedData = data.bot.trim()
 
         typeText(messageDiv, parsedData)
     } else {
