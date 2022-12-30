f9a1cbb33a6bc31e049fe7cea21ee6d2163d1b36
robson <robsonmesquita-2011@outlook.com>
2022-12-30 06:58:45 -0300

    update the server url

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('https://codex-jffo.onrender.com/', {
+    const response = await fetch('https://codex-jffo.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
